e93ef2df112b6bbd1c8959ede78f36c2a54be45c
George Kuan <commonlisp@gmail.com>
2014-12-05 07:27:10 -0800

    Makefile, getting atd to conform, and integrating with netclient in ocamlnet

--- api.ml
+++ api.ml
@@ -1,23 +1,21 @@
 (* api.ml -- Kubernetes interface for OCaml *)
 open Http_client.Convenience
 
-module Api(param : sig val server_url : string end) = 
+module Api = functor(Param : sig val server_url : string end) ->
 struct 
   module Pod = struct
-     let resource_base "/pods"
+     let resource_base = "/pods"
      let resource = resource_base ^ "/"
-     let nextId = let x = ref 0 in fun () -> x := !x + 1; (string_of_int x);;
+     let nextId = let x = ref 0 in fun () -> x := !x + 1; (string_of_int !x);;
 
      let get_all () = 
-        http_get (server_url ^ resource_base)
+        http_get (Param.server_url ^ resource_base)
  
      let get id = 
-        http_get (server_url ^ resource ^ id)
+        http_get (Param.server_url ^ resource ^ id)
  
      let make url pod =
-        http_put (server_url ^ resource ^ nextId()) (Kubernetes_types_j.string_of_pod pod)
+        http_put (Param.server_url ^ resource ^ nextId()) (Kubernetes_types.string_of_pod pod)
   end
 end
-
-
 
